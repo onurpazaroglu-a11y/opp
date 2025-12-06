b6bfec5e0b52536ddc951f468a0a9324002b5c21
[dyad] <onurpazaroglu@gmail.com>
2025-12-06 19:50:54 +0300

    [dyad] Ana sayfaya portföy bölümleri eklendi. - wrote 1 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
 import { HeroSection } from "@/components/HeroSection";
+import { PortfolioSection } from "@/components/PortfolioSection";
+import { categories } from "@/data/portfolio";
 
 export default function Home() {
   return (
@@ -7,7 +9,9 @@ export default function Home() {
       <HeroSection />
 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        {/* Portfolio Sections were here, now removed */}
+        {categories.map((category) => (
+          <PortfolioSection key={category.name} category={category.name} />
+        ))}
       </div>
       
       <div className="mt-auto">
